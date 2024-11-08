57292bf6b3c7e6e9539a609bbd5b149a06a38f3b
rebcs <rebecaciriaca@gmail.com>
2024-11-08 15:50:31 -0300

    estilizando botões página adm e acrescentando imagens página sobre nós

--- client/src/pages/AdminDashboard.jsx
+++ client/src/pages/AdminDashboard.jsx
@@ -1,11 +1,15 @@
 // AdminDashboard.js
-import React, { useState } from "react";
-import { useNavigate } from "react-router-dom";
-import AdicionarPontoPopup from "./AdicionarPontoPopup"; // Popup para adicionar pontos
-import ValidarEmpresaPopup from "./ValidarEmpresaPopup"; // Popup para validar empresas
-import ValidarONGPopup from "./ValidarONGPopup"; // Novo popup para validar ONGs
+import React, { useState } from "react"
+import { useNavigate } from "react-router-dom"
+import AdicionarPontoPopup from "./AdicionarPontoPopup" // Popup para adicionar pontos
+import ValidarEmpresaPopup from "./ValidarEmpresaPopup" // Popup para validar empresas
+import ValidarONGPopup from "./ValidarONGPopup" // Novo popup para validar ONGs
 import Logo from '../assets/images/logo.png'
-import { Link } from "react-router-dom";
+import Sugeridos from '../assets/images/pontos-sugeridos.png'
+import AddPonto from '../assets/images/adicionar-ponto.png'
+import ValidarEmpresa from '../assets/images/validar-empresa.png'
+import ValidarONG from '../assets/images/validar-ong.png'
+import { Link } from "react-router-dom"
 import '../assets/styles/AdminDashboard.css'
 
 const AdminDashboard = () => {
@@ -33,17 +37,57 @@ const AdminDashboard = () => {
     return (
 
         <>
-            <img className="logoHeader" src={Logo} alt="Logo" />
-            <a id="btnContact">
+        <nav className="menu_navegacao">
+            <Link to="/">
+                <img className="logoHeader" src={Logo} alt="Logo" />
+            </Link>            
+            <a className="btn-sair">
                 <Link to="/Login2">Sair</Link>
             </a>
+        </nav>
 
+            <h2>Painel de Gerenciamento do Administrador</h2>
             <div className="dashboard-container">
-                <h2>Painel de Gerenciamento do Administrador</h2>
-                <button onClick={handleSuggestedPointsClick}>PONTOS SUGERIDOS</button>
-                <button onClick={handleAdicionarPontoClick}>ADICIONAR PONTO</button>
-                <button onClick={handleValidarEmpresaClick}>VALIDAR EMPRESA</button>
-                <button onClick={handleValidarONGClick}>VALIDAR ONG</button>
+
+                <button className="btn-dashboard ">
+                    <img
+                        src={Sugeridos}
+                        alt="Ícone do botão"
+                        className="button-icon"
+                        onClick={handleSuggestedPointsClick}
+                    />
+                    Pontos Sugeridos
+                </button>
+
+                <button className="btn-dashboard ">
+                    <img
+                        src={AddPonto}
+                        alt="Ícone do botão"
+                        className="button-icon"
+                        onClick={handleAdicionarPontoClick}
+                    />
+                    Adicionar Ponto
+                </button>
+
+                <button className="btn-dashboard ">
+                    <img
+                        src={ValidarEmpresa}
+                        alt="Ícone do botão"
+                        className="button-icon"
+                        onClick={handleValidarEmpresaClick}
+                    />
+                    Validar Empresa
+                </button>
+
+                <button className="btn-dashboard ">
+                    <img
+                        src={ValidarONG}
+                        alt="Ícone do botão"
+                        className="button-icon"
+                        onClick={handleValidarONGClick}
+                    />
+                    Validar ONG
+                </button>
 
                 {/* Renderiza o popup de adicionar ponto */}
                 {showAdicionarPontoPopup && (
