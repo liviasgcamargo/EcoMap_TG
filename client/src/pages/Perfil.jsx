eeb9caa470cf9928fdd588f61daf674ea4cef705
rebcs <rebecaciriaca@gmail.com>
2024-11-13 11:35:57 -0300

    Perfil do usuário finalizada

--- client/src/pages/Perfil.jsx
+++ client/src/pages/Perfil.jsx
@@ -149,6 +149,8 @@ import api from "../services/api";
 import { useNavigate } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Footer from "../components/Footer";
+import IconePerfil from "../assets/images/imagem-perfil.svg"
+import "../assets/styles/Perfil.css"
 
 const Perfil = () => {
     const [perfil, setPerfil] = useState(null);
@@ -218,150 +220,151 @@ const Perfil = () => {
 
     return (
         <div>
-            <Navbar/>
+            <Navbar />
 
             <h2>Perfil de {perfil.fk_id_categoria === 1 ? "Empresa" : "ONG"}</h2>
-        <div className="perfil-container">
+            <div className="perfil-container">
 
-            <div className="perfil-info">
-                <div className='info-cadastro-principais'>
-                <label>Email:</label>
-                <input 
-                    type="email" 
-                    name="email" 
-                    value={perfil.email || ''} 
-                    onChange={handleChange} 
-                    disabled={!editMode} 
-                />
+                <div className="perfil-info">
+                    <img className="icone-perfil" src={IconePerfil} alt="" />
+                    <div className='info-cadastro-principais'>
+                        <label>Email:</label>
+                        <input
+                            type="email"
+                            name="email"
+                            value={perfil.email || ''}
+                            onChange={handleChange}
+                            disabled={!editMode}
+                        />
 
-                <label>Senha:</label>
-                <input 
-                    type="password" 
-                    name="senha" 
-                    placeholder="Nova senha" 
-                    onChange={handleChange} 
-                    disabled={!editMode} 
-                />
-                </div>
-
-                <div className="info-cadastro-principais">
-                <label>Nome da Organização:</label>
-                <input 
-                    type="text" 
-                    name="nome_org" 
-                    value={perfil.nome_org || ''} 
-                    onChange={handleChange} 
-                    disabled={!editMode} 
-                />
-
-                <label>CNPJ:</label>
-                <input 
-                    type="text" 
-                    name="CNPJ" 
-                    value={perfil.CNPJ || ''} 
-                    onChange={handleChange} 
-                    disabled={!editMode} 
-                />
-                </div>
-                
-<div  className="info-cadastro-principais">
-                <label>Telefone:</label>
-                <input 
-                    type="text" 
-                    name="telefone" 
-                    value={perfil.telefone || ''} 
-                    onChange={handleChange} 
-                    disabled={!editMode} 
-                />
-
-                <label>Descrição:</label>
-                <textarea 
-                    name="descricao" 
-                    value={perfil.descricao || ''} 
-                    onChange={handleChange} 
-                    disabled={!editMode} 
-                />
-</div>
-
-<div className="info-cadastro-principais">
-                <label>Tipo de Serviço (Retira no local ou Não Retira):</label>
-                <select 
-                    name="tipo_servico" 
-                    value={perfil.tipo_servico || ''} 
-                    onChange={handleChange} 
-                    disabled={!editMode}
-                >
-                    <option value="Retira no Local">Retira no Local</option>
-                    <option value="Não Retira">Não Retira</option>
-                </select>
-
-                <label>Endereço:</label>
-                <input 
-                    type="text" 
-                    name="endereco" 
-                    value={perfil.endereco || ''} 
-                    onChange={handleChange} 
-                    disabled={!editMode} 
-                />
-</div>
-
-<div className="info-cadastro-principais">
-
-                <label>CEP:</label>
-                <input 
-                    type="text" 
-                    name="cep" 
-                    value={perfil.cep || ''} 
-                    onChange={handleChange} 
-                    disabled={!editMode} 
-                />
-
-                <label>Cidade:</label>
-                <input 
-                    type="text" 
-                    name="cidade" 
-                    value={perfil.cidade || ''} 
-                    onChange={handleChange} 
-                    disabled={!editMode} 
-                />
-</div>
-
-<div className="info-cadastro-principais">
-                <label>Estado:</label>
-                <input 
-                    type="text" 
-                    name="estado" 
-                    value={perfil.estado || ''} 
-                    onChange={handleChange} 
-                    disabled={!editMode} 
-                />
-
-                <label>Tipos de Material Aceito:</label>
-                <input 
-                    type="text" 
-                    name="materiais" 
-                    placeholder="Ex: Papelão, Plástico" 
-                    value={perfil.materiais || ''} 
-                    onChange={handleChange} 
-                    disabled={!editMode} 
-                />
-</div>
-
-                {editMode ? (
-                    <div>
-                        <button onClick={handleSave}>Salvar</button>
-                        <button onClick={handleCancel}>Cancelar</button>
+                        <label>Senha:</label>
+                        <input
+                            type="password"
+                            name="senha"
+                            placeholder="Nova senha"
+                            onChange={handleChange}
+                            disabled={!editMode}
+                        />
                     </div>
-                ) : (
-                    <button onClick={() => setEditMode(true)}>Editar Perfil</button>
-                )}
 
-                <button onClick={handleDelete}>Excluir Conta</button>
+                    <div className="info-cadastro-principais">
+                        <label>Nome da Organização:</label>
+                        <input
+                            type="text"
+                            name="nome_org"
+                            value={perfil.nome_org || ''}
+                            onChange={handleChange}
+                            disabled={!editMode}
+                        />
+
+                        <label>CNPJ:</label>
+                        <input
+                            type="text"
+                            name="CNPJ"
+                            value={perfil.CNPJ || ''}
+                            onChange={handleChange}
+                            disabled={!editMode}
+                        />
+                    </div>
+
+                    <div className="info-cadastro-principais">
+                        <label>Telefone:</label>
+                        <input
+                            type="text"
+                            name="telefone"
+                            value={perfil.telefone || ''}
+                            onChange={handleChange}
+                            disabled={!editMode}
+                        />
+
+                        <label>Descrição:</label>
+                        <textarea
+                            name="descricao"
+                            value={perfil.descricao || ''}
+                            onChange={handleChange}
+                            disabled={!editMode}
+                        />
+                    </div>
+
+                    <div className="info-cadastro-principais">
+                        <label>Tipo de Serviço (Retira no local ou Não Retira):</label>
+                        <select
+                            name="tipo_servico"
+                            value={perfil.tipo_servico || ''}
+                            onChange={handleChange}
+                            disabled={!editMode}
+                        >
+                            <option value="Retira no Local">Retira no Local</option>
+                            <option value="Não Retira">Não Retira</option>
+                        </select>
+
+                        <label>Endereço:</label>
+                        <input
+                            type="text"
+                            name="endereco"
+                            value={perfil.endereco || ''}
+                            onChange={handleChange}
+                            disabled={!editMode}
+                        />
+                    </div>
+
+                    <div className="info-cadastro-principais">
+
+                        <label>CEP:</label>
+                        <input
+                            type="text"
+                            name="cep"
+                            value={perfil.cep || ''}
+                            onChange={handleChange}
+                            disabled={!editMode}
+                        />
+
+                        <label>Cidade:</label>
+                        <input
+                            type="text"
+                            name="cidade"
+                            value={perfil.cidade || ''}
+                            onChange={handleChange}
+                            disabled={!editMode}
+                        />
+                    </div>
+
+                    <div className="info-cadastro-principais">
+                        <label>Estado:</label>
+                        <input
+                            type="text"
+                            name="estado"
+                            value={perfil.estado || ''}
+                            onChange={handleChange}
+                            disabled={!editMode}
+                        />
+
+                        <label>Tipos de Material Aceito:</label>
+                        <input
+                            type="text"
+                            name="materiais"
+                            placeholder="Ex: Papelão, Plástico"
+                            value={perfil.materiais || ''}
+                            onChange={handleChange}
+                            disabled={!editMode}
+                        />
+                    </div>
+
+                    {editMode ? (
+                        <div>
+                            <button className="btn-perfil" onClick={handleSave}>Salvar</button>
+                            <button className="btn-perfil" onClick={handleCancel}>Cancelar</button>
+                        </div>
+                    ) : (
+                        <button className="btn-perfil" onClick={() => setEditMode(true)}>Editar Perfil</button>
+                    )}
+
+                    <button className="btn-perfil" onClick={handleDelete}>Excluir Conta</button>
+                </div>
             </div>
+            <Footer />
         </div>
-        <Footer/>
-        </div>
-       
+
 
     );
 };
