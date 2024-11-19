8f696f68963493ddd29ef09b9976ac7efbbd95ba
liviasgcamargo <livia.camargo01@fatec.sp.gov.br>
2024-11-19 20:19:26 -0300

    credenciais backend

--- backend/index.js
+++ backend/index.js
@@ -16,17 +16,25 @@ dotenv.config();
 const app = express();
 
 const db = mysql.createConnection({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_NAME
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "ecomap",
 });
 
 const bd = mysql.createPool({
-  host: process.env.DB_HOST,
-  user: process.env.DB_USER,
-  password: process.env.DB_PASS,
-  database: process.env.DB_NAME
+  // host: process.env.DB_HOST,
+  // user: process.env.DB_USER,
+  // password: process.env.DB_PASS,
+  // database: process.env.DB_NAME
+  host: "localhost",
+  user: "root",
+  password: "123456",
+  database: "ecomap",
 });
 
 export default bd;
