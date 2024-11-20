a8992837b17850692325976d74a4d5aafcbfdf74
liviasgcamargo <livia.camargo01@fatec.sp.gov.br>
2024-11-19 21:05:41 -0300

    colocando exemplo de preencher popup buscar

--- client/src/components/BuscarPontoPopUp.jsx
+++ client/src/components/BuscarPontoPopUp.jsx
@@ -55,7 +55,7 @@ const BuscarPontoPopUp = ({ onClose }) => {
                     type="text"
                     value={address}
                     onChange={(e) => setAddress(e.target.value)}
-                    placeholder="Digite o endereço"
+                    placeholder="Exemplo: Rua Das Margaridas nº12, Jardim das Flores"
                 />
                 <label>Raio de Distância (km):</label>
                 <select value={radius} onChange={(e) => setRadius(Number(e.target.value))}>
